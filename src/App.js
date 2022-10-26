80aeda63035c04ce4cd2055621ecf497ae9aad39
Abai <abai.kuseinov.kg@gmail.com>
2022-10-26 13:57:44 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [items, setItem] = useState([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/photos?_limit=10')
+      .then((response) => response.json())
+      .then((json) => setItem(json));
+  }, [items]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {items.map((item) => {
+        return (
+          <div className="Users" key={item.id}>
+            <p> {item.id}</p>
+            <p>{item.title}</p>
+          <img src={item.thumbnailUrl} alt="" />
+          </div>
+        );
+      })}
     </div>
   );
 }
